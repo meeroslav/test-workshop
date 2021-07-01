381b2154c34fd9c5754f5825e495943feea500fe
Miroslav Jonas <missing.manual@gmail.com>
2021-07-01 20:47:41 +0200

    lab 9

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { Game } from '@bg-hoard/util-interface';
 
-const games = [
+const games: Game[] = [
   {
     id: 'settlers-in-the-can',
     name: 'Settlers in the Can',
@@ -8,7 +9,7 @@ const games = [
     description:
       'Help your bug family claim the best real estate in a spilled can of beans.',
     price: 35,
-    rating: Math.random()
+    rating: Math.random(),
   },
   {
     id: 'chess-pie',
@@ -16,7 +17,7 @@ const games = [
     image: 'https://media.giphy.com/media/iCZyBnPBLr0dy/giphy.gif',
     description: 'A circular game of Chess that you can eat as you play.',
     price: 15,
-    rating: Math.random()
+    rating: Math.random(),
   },
   {
     id: 'purrfection',
@@ -24,17 +25,17 @@ const games = [
     image: 'https://media.giphy.com/media/12xMvwvQXJNx0k/giphy.gif',
     description: 'A cat grooming contest goes horribly wrong.',
     price: 45,
-    rating: Math.random()
-  }
+    rating: Math.random(),
+  },
 ];
 
 @Injectable()
 export class AppService {
-  getAllGames() {
+  getAllGames(): Game[] {
     return games;
   }
 
-  getGame(id: string) {
-    return games.find(game => game.id === id);
+  getGame(id: string): Game {
+    return games.find((game) => game.id === id);
   }
 }
--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -1,19 +1,18 @@
 import { Component } from '@angular/core';
 import { formatRating } from '@bg-hoard/store/util-formatters';
 import { HttpClient } from '@angular/common/http';
+import { Game } from '@bg-hoard/util-interface';
 
 @Component({
   selector: 'bg-hoard-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Board Game Hoard';
-  games = this.http.get<any[]>('/api/games');
+  games = this.http.get<Game[]>('/api/games');
 
   formatRating = formatRating;
 
-  constructor(private readonly http: HttpClient) {
-
-  }
+  constructor(private readonly http: HttpClient) {}
 }
