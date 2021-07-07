17c643ad6517b2c751290e98d280e79719c41e1a
Miroslav Jonas <missing.manual@gmail.com>
2021-07-07 22:16:44 +0200

    lab 15-2

--- libs/store/util-formatters/src/lib/store-util-formatters.spec.ts
+++ libs/store/util-formatters/src/lib/store-util-formatters.spec.ts
@@ -1,7 +1,7 @@
-import { storeUtilFormatters } from './store-util-formatters';
+import { formatRating } from './store-util-formatters';
 
-describe('storeUtilFormatters', () => {
+describe('formatRating', () => {
   it('should work', () => {
-    expect(storeUtilFormatters()).toEqual('store-util-formatters');
+    expect(formatRating(0.23234234)).toEqual('2.3 / 10');
   });
 });
