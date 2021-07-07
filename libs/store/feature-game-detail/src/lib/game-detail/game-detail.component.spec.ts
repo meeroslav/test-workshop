7c643b4bc47b333aa400f5c1a0ec23e5a0458d4c
Miroslav Jonas <missing.manual@gmail.com>
2021-07-07 20:19:22 +0200

    fix: tests

--- libs/store/feature-game-detail/src/lib/game-detail/game-detail.component.spec.ts
+++ libs/store/feature-game-detail/src/lib/game-detail/game-detail.component.spec.ts
@@ -1,6 +1,9 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { GameDetailComponent } from './game-detail.component';
+import { CommonModule } from '@angular/common';
 
 describe('GameDetailComponent', () => {
   let component: GameDetailComponent;
@@ -8,9 +11,10 @@ describe('GameDetailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GameDetailComponent ]
+      imports: [RouterTestingModule, HttpClientTestingModule, CommonModule],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
