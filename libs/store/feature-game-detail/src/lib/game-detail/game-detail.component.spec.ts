fc470be69800ee29bf0c6ec424988d7e63d6b8b1
Miroslav Jonas <missing.manual@gmail.com>
2021-07-07 20:36:08 +0200

    fix: failing test

--- libs/store/feature-game-detail/src/lib/game-detail/game-detail.component.spec.ts
+++ libs/store/feature-game-detail/src/lib/game-detail/game-detail.component.spec.ts
@@ -3,7 +3,6 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { GameDetailComponent } from './game-detail.component';
-import { CommonModule } from '@angular/common';
 
 describe('GameDetailComponent', () => {
   let component: GameDetailComponent;
@@ -11,8 +10,9 @@ describe('GameDetailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, HttpClientTestingModule, CommonModule],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      declarations: [GameDetailComponent]
     })
       .compileComponents();
   });
